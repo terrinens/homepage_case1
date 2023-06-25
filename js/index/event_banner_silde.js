c88eeb4bb3cc0bdfa233e5134804e1ed796db7d1
Dongchuel <terrinens@gamil.com>
2023-06-25 21:42:22 +0900

    23-06-25

---
+++ js/index/event_banner_silde.js
@@ -0,0 +1,21 @@
+$(document).ready (function () {
+    
+    var banner_img = [
+        {
+            img : " .\img\event_banner\event_banner01.jpg",
+            link : "#"
+        },
+        {
+            img : " .\img\event_banner\event_banner02.jpg",
+            link : "#"
+        },
+        {
+            img : " .\img\event_banner\event_banner03.jpg",
+            link : "#"
+        }
+    ]
+
+    (function () {
+        
+    });
+});
